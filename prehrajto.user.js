347f6402a17698e5e222258ee6de13b405d2fb63
bekucera <bekucera@gmail.com>
2024-05-10 11:54:04 +0200

    formating and removed event

--- prehrajto.user.js
+++ prehrajto.user.js
@@ -8,12 +8,9 @@
 // @description 5/10/2024, 10:46:53 AM
 // ==/UserScript==
 
-window.addEventListener('load', function () {
-    h1 = document.getElementsByTagName("h1")[0];
-    buttons = "";
-    sources.videos.map((item) => {
-        buttons += `<a src="${item.src}">${item.res}p</a><br>`;
-    });
-    h1.innerHTML = `${h1.innerHTML}<br/>${buttons}`;
-    console.log();
-}, false);
+h1 = document.getElementsByTagName("h1")[0];
+buttons = "";
+sources.videos.map((item) => {
+    buttons += `<a src="${item.src}">${item.res}p</a><br>`;
+});
+h1.innerHTML = `${h1.innerHTML}<br/>${buttons}`;
