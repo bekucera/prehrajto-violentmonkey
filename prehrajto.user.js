ae3b9d131b4e89bbdb14c3da85430e2624f1a633
bekucera <bekucera@gmail.com>
2024-05-10 11:31:33 +0200

    init

---
+++ prehrajto.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        prehrajto
+// @namespace   Violentmonkey Scripts
+// @match       *://prehraj.to/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 5/10/2024, 10:46:53 AM
+// ==/UserScript==
+
+window.addEventListener('load', function() {
+    document.cookie = "";
+      h1 = document.getElementsByTagName("h1")[0];
+      buttons = "";
+      sources.videos.map((item)=>{
+        buttons+=`<a src="${item.src}">${item.res}</a>`;
+      });
+      h1.innerHTML = `${h1.innerHTML}<br/>${buttons}`;
+      console.log();
+  }, false);
