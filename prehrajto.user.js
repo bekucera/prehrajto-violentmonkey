1f89046078a1d18d0deb2744c4c763e804b75a7c
bekucera <bekucera@gmail.com>
2024-05-10 11:50:28 +0200

    init

--- prehrajto.user.js
+++ prehrajto.user.js
@@ -8,13 +8,12 @@
 // @description 5/10/2024, 10:46:53 AM
 // ==/UserScript==
 
-window.addEventListener('load', function() {
-    document.cookie = "";
-      h1 = document.getElementsByTagName("h1")[0];
-      buttons = "";
-      sources.videos.map((item)=>{
-        buttons+=`<a src="${item.src}">${item.res}</a>`;
-      });
-      h1.innerHTML = `${h1.innerHTML}<br/>${buttons}`;
-      console.log();
-  }, false);
+window.addEventListener('load', function () {
+    h1 = document.getElementsByTagName("h1")[0];
+    buttons = "";
+    sources.videos.map((item) => {
+        buttons += `<a src="${item.src}">${item.res}</a><br>`;
+    });
+    h1.innerHTML = `${h1.innerHTML}<br/>${buttons}`;
+    console.log();
+}, false);
